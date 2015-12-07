95fef9393040f42b6120ca90eedd5f6c3fffa3fa
Zebin Xie <zebin.xie@gmail.com>
2015-12-06 20:57:11 -0600

    textRank, include stop_word removal and stemming

--- TextRank.py
+++ TextRank.py
@@ -5,8 +5,21 @@ import networkx as nx
 import numpy as np
 
 from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktParameters
-from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer, TfidfVectorizer
+from nltk.stem.snowball import SnowballStemmer
 
+
+def stemSen(sentence):
+    stemmer = SnowballStemmer("english")
+    words = sentence.split(" ")
+    rvSen = ""
+    for word in words:
+        rvSen += stemmer.stem(word) + " "
+    return rvSen
+
+class customCountVectorizer(CountVectorizer):
+    def build_preprocessor(self):
+        return stemSen
 
 class TextRank(object):
     def __init__(self):
@@ -30,10 +43,21 @@ class TextRank(object):
         sentences = sentence_tokenizer.tokenize(document)
 
 
+        # FEATURE: stem words, remove stop words
+
+
         # == count words for each sentence ==
-        wordCounter = CountVectorizer()
+        # wordCounter = CountVectorizer()                                            # approach 0: non stop_word & stem
+        # wordCounter = CountVectorizer(stop_words='english')                       # approach 1: only stop_word
+        wordCounter = CountVectorizer(stop_words='english', preprocessor=stemSen)   # approach 2.a: pass function
+        # wordCounter = customCountVectorizer(stop_words='english')                 # approach 2.b: custom class
         count_matrix = wordCounter.fit_transform(sentences)
         normalized_matrix = TfidfTransformer().fit_transform(count_matrix)
+
+
+        # wordCounter = TfidfVectorizer()
+        # normalized_matrix = wordCounter.fit_transform(sentences)
+
 
         # == similarity among sentences ==
         similarity_graph = normalized_matrix * normalized_matrix.T
@@ -43,11 +67,12 @@ class TextRank(object):
 
 
         with open("doc_textrank.txt", "w") as lofile:
-            for i in range(0, 3):
+            for i in range(0, len(orderedSentences)):
                 lofile.write(orderedSentences[i][1].encode('ascii', 'ignore'))
                 lofile.write("\n")
 
-        print ""
+
+        # print count_matrix
 
 
 #** Run the code **#
@@ -56,3 +81,4 @@ def test():
     solution.textrank()
 
 test()
+# print stemSen("having having done did")
